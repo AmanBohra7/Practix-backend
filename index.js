20fd5f37857ef578bf56f9c71eadd5d12072a872
AmanBohra7 <52762311+AmanBohra7@users.noreply.github.com>
2021-06-14 14:12:59 +0530

    adding testing code

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5001;
 app.use(cors())
 app.use(express.json())
 
+// testing
+app.post('/test',(req,res) => {
+    res.status(200).json({"message":"You are ready to go!"});
+})
+
 // create new user
 app.post('/createuser',(req,res) => {
     console.log(req.body);
