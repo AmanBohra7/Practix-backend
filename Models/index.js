ec80c04d3e7166fd4a32050144afd79bc2fd98fb
AmanBohra7 <52762311+AmanBohra7@users.noreply.github.com>
2021-06-15 01:07:22 +0530

    Updating uri for Mognodb ATLAS

--- Models/index.js
+++ Models/index.js
@@ -2,13 +2,19 @@ const User = require('./User');
 const UserExp = require('./UserExp');
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://localhost:27017/practix';
+// const uri = 'mongodb://localhost:27017/practix';
+const uri = 'mongodb+srv://sanmati_labs:OzaEKnOP197ujI5H@cluster0.lqws3.mongodb.net/Cluster0?retryWrites=true&w=majority'
 
-const Connection = mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:false})
+const Connection = mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:false}).then( () => {
+    console.log('Connected to database ')
+})
+.catch( (err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+})
 const connection = mongoose.connection;
 connection.once('open',()=>{
     console.log("MongoDB database connection established successfully!")
-})
+});
 
 let Model = {
     Connection,
